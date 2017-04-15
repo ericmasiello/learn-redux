c4aa86a2ada3c9968eb748901c885a076e887e69
Eric Masiello <eric.j.masiello@gmail.com>
2017-04-15 19:50:28 -0400

    fix path

--- src/util/nytFetch.js
+++ src/util/nytFetch.js
@@ -1,4 +1,4 @@
-import { API_KEY } from '../../config/apikey';
+import { API_KEY } from '../config/apikey';
 
 function nytFetch(endpoint, params) {
   return fetch(`http://api.nytimes.com/svc/topstories/v2/${endpoint}.json?api-key=${API_KEY}`)
