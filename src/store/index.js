33b1a1ce60ea01d548642cd40d6792382cf48b19
Eric Masiello <eric.j.masiello@gmail.com>
2017-04-15 20:25:46 -0400

    Add redux logger

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,14 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import newsReducer from '../reducers/newsReducer';
+import logger from 'redux-logger';
 
 const store = createStore(
   combineReducers({
     news: newsReducer,
     searchTerm: () => '',
     bookmarks: () => [],
-  })  
+  }),
+  applyMiddleware(logger),
 );
 
 export default store;
