f8cb34a1f913f3eed541befca1519bac845f1e65
Eric Masiello <eric.j.masiello@gmail.com>
2017-04-15 21:43:32 -0400

    Add selector

--- src/containers/NewsArchiveContainer.js
+++ src/containers/NewsArchiveContainer.js
@@ -1,10 +1,11 @@
 import { connect } from 'react-redux';
 import { loadNews } from '../actions/newsActions';
 import NewsArchive from '../components/NewsArchive';
+import { searchNewsSelector } from '../selectors/newsSelectors';
 
 function mapStateToProps(state) {
   return {
-    news: state.news,
+    news: searchNewsSelector(state),
     isLoading: state.isLoading,
   };
 }
