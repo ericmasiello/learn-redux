b1006af4542079c3b75fcda2c0a96892ff6c1656
Eric Masiello <eric.j.masiello@gmail.com>
2017-04-21 09:23:07 -0400

    Refactor to use Class component. Will save us refactoring later

--- src/components/NewsArchive.js
+++ src/components/NewsArchive.js
@@ -1,23 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Article from './Article';
 import './NewsArchive.css';
 
-function NewsArchive(props) {
-  const { news } = props;
-  
-  return (
-    <ul className="news-archive">
-      {news.map((article, i) => (
-        <li key={article.title}>
-          <Article
-            {...article}
-            index={i}
-          />
-        </li>
-      ))}
-    </ul>
-  )
+class NewsArchive extends Component {
+  render() {
+    const { news } = this.props;
+    return (
+      <ul className="news-archive">
+        {news.map((article, i) => (
+          <li key={article.title}>
+            <Article
+              {...article}
+              index={i}
+            />
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 NewsArchive.propTypes = {
